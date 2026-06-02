6e403165e4f9ca1f6c35502e65777f3e99b36a3b
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-02 13:04:54 +0200

    add ManyToMany relation between Pizza and Ingredient

--- readme.md
+++ readme.md
@@ -71,3 +71,13 @@ L’offerta speciale avrà :
 La pagina di dettaglio della singola pizza mostrerà l’elenco delle offerte collegate e avrà un bottone “Crea nuova offerta speciale” per aggiungerne una nuova.
 
 Accanto ad ogni offerta speciale è previsto un bottone che mi porterà a una pagina per modificarla.
+
+### Step 6
+
+Aggiungiamo una nuova fuzionalità per la nostra pizzeria: gli ingredienti!
+
+Ogni pizza può avere più ingredienti, e ogni ingrediente può essere collegato a più pizze.
+
+Prevediamo quindi una pagina per mostrare l’elenco di tutti gli ingredienti che utilizziamo nella nostra pizzeria che permetta anche di crearne di nuovi (e di cancellarli).
+
+Nella pagina di creazione (e modifica) della singola pizza dobbiamo dare la possibilità di collegare uno o più ingredienti.
--- src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
@@ -40,6 +43,10 @@ public class Pizza {
     @OneToMany(mappedBy = "pizza")
     // @OneToMany(mappedBy = "pizza", cascade = { CascadeType.REMOVE })
     private List<Promotion> promotions;
+
+    @ManyToMany
+    @JoinTable(name = "ingredient_pizza", joinColumns = @JoinColumn(name = "pizza_id"), inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
+    private List<Ingredient> ingredients;
 
     public Integer getId() {
         return id;
@@ -92,5 +99,13 @@ public class Pizza {
 
     public void setPromotions(List<Promotion> promotions) {
         this.promotions = promotions;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
